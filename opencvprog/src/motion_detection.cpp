19d8e65f399704bea0d978d43d4760739ace9ab2
amthesis <cedric@cedricve.me>
2013-06-17 16:23:24 +0200

    added motion detection

--- opencvprog/src/motion_detection.cpp
+++ opencvprog/src/motion_detection.cpp
@@ -51,14 +51,6 @@ bool saveImg(Mat image, const string DIRECTORY, const string EXTENSION, const ch
     return imwrite(ss.str().c_str(), image);
 }
 
-bool saveImg_norm(Mat image, const string DIRECTORY, const string NAME,  const string EXTENSION){
-    stringstream ss;
-    ss.str("");
-    ss << DIRECTORY << NAME << EXTENSION;
-
-    return imwrite(ss.str().c_str(), image);
-}
-
 int main (int argc, char * const argv[]){
     
     // const
@@ -83,9 +75,11 @@ int main (int argc, char * const argv[]){
     Mat current_frame = cvQueryFrame(camera);
     Mat prev_frame = cvQueryFrame(camera);
     Mat original2;
-cvtColor(current_frame, current_frame, CV_RGB2GRAY);
+	
+	cvtColor(current_frame, current_frame, CV_RGB2GRAY);
     cvtColor(prev_frame, prev_frame, CV_RGB2GRAY);
     cvtColor(next_frame, next_frame, CV_RGB2GRAY);
+	
     Mat d1, d2, result;
     int window = 200;
 
@@ -94,22 +88,25 @@ cvtColor(current_frame, current_frame, CV_RGB2GRAY);
     Scalar sc2(0,0,255);
     
     while (true){
-        
+        // Calculate differences between the images
         absdiff(next_frame, current_frame, d1);
         absdiff(current_frame, prev_frame, d2);
         bitwise_xor(d1, d2, result);
         
         int middle_y = result.rows/2;
         int middle_x = result.cols/2;
-	int count = 0;
+		int count = 0;
+		
         threshold(result, result, 50, 255, CV_THRESH_BINARY);
         Mat kernel_dil = getStructuringElement(MORPH_RECT, Size(7,7));
         erode(result, result, kernel_dil);
-        for(int j = middle_y-window+50; j < middle_y+window+100; j++){
-            for(int i = 500; i < middle_x+window*3; i++){
+			
+		
+        for(int j = middle_y-window; j < middle_y+window; j++){
+            for(int i = middle_x-window; i < middle_x+window; i++){
                 if(result.at<int>(j,i)>0)
                 {
-	            count++;
+	    			count++;
                     Point x((i-960)*4-30,j-30);
                     Point y((i-960)*4+30,j+30);
                     rectangle(original,x,y,sc2,1);
@@ -119,7 +116,8 @@ cvtColor(current_frame, current_frame, CV_RGB2GRAY);
 
         }
         
-        //imshow("original", original);
+		// If a lot of changes happened on multiple rows
+		// we assume something big changed.
         if(count>=7)
             saveImg(original,DIR,EXT,DIR_FORMAT.c_str(),FILE_FORMAT.c_str());
         
