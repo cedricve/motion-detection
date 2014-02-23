f77b862c933da452b99506907f4083f71a992bf8
cedricve <info@cedricve.me>
2014-02-23 14:59:40 +0100

    added region

---
+++ motion_with_region_src/src/motion_detection.cpp
@@ -0,0 +1,216 @@
+//
+//  Created by Cedric Verstraeten on 18/02/14.
+//  Copyright (c) 2014 Cedric Verstraeten. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <time.h>
+#include <dirent.h>
+#include <sstream>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "tinyxml.h"
+
+using namespace std;
+using namespace cv;
+
+// Check if the directory exists, if not create it
+// This function will create a new directory if the image is the first
+// image taken for a specific day
+inline void directoryExistsOrCreate(const char* pzPath)
+{
+    DIR *pDir;
+    // directory doesn't exists -> create it
+    if ( pzPath == NULL || (pDir = opendir (pzPath)) == NULL)
+        mkdir(pzPath, 0777);
+    // if directory exists we opened it and we
+    // have to close the directory again.
+    else if(pDir != NULL)
+        (void) closedir (pDir);
+}
+
+// When motion is detected we write the image to disk
+//    - Check if the directory exists where the image will be stored.
+//    - Build the directory and image names.
+int incr = 0;
+inline bool saveImg(Mat image, const string DIRECTORY, const string EXTENSION, const char * DIR_FORMAT, const char * FILE_FORMAT)
+{
+    stringstream ss;
+    time_t seconds;
+    struct tm * timeinfo;
+    char TIME[80];
+    time (&seconds);
+    // Get the current time
+    timeinfo = localtime (&seconds);
+    
+    // Create name for the date directory
+    strftime (TIME,80,DIR_FORMAT,timeinfo);
+    ss.str("");
+    ss << DIRECTORY << TIME;
+    directoryExistsOrCreate(ss.str().c_str());
+    ss << "/cropped";
+    directoryExistsOrCreate(ss.str().c_str());
+    
+    // Create name for the image
+    strftime (TIME,80,FILE_FORMAT,timeinfo);
+    ss.str("");
+    if(incr < 100) incr++; // quick fix for when delay < 1s && > 10ms, (when delay <= 10ms, images are overwritten)
+    else incr = 0;
+    ss << DIRECTORY << TIME << static_cast<int>(incr) << EXTENSION;
+    return imwrite(ss.str().c_str(), image);
+}
+
+// Check if there is motion in the result matrix
+// count the number of changes and return.
+inline int detectMotion(const Mat & motion, Mat & result, Mat & result_cropped,
+                        vector<Point2f> & region,
+                        int max_deviation,
+                        Scalar & color)
+{
+    // calculate the standard deviation
+    Scalar mean, stddev;
+    meanStdDev(motion, mean, stddev);
+    // if not to much changes then the motion is real (neglect agressive snow, temporary sunlight)
+    if(stddev[0] < max_deviation)
+    {
+        int number_of_changes = 0;
+        int min_x = motion.cols, max_x = 0;
+        int min_y = motion.rows, max_y = 0;
+        // loop over image and detect changes
+        int x, y, size = region.size();
+        
+        for(int i = 0; i < size; i++){ // loop over region
+            x = region[i].x;
+            y = region[i].y;
+            if(static_cast<int>(motion.at<uchar>(y,x)) == 255)
+            {
+                number_of_changes++;
+                if(min_x>x) min_x = x;
+                if(max_x<x) max_x = x;
+                if(min_y>y) min_y = y;
+                if(max_y<y) max_y = y;
+            }
+        }
+        if(number_of_changes){
+            Point x(min_x-10,min_y-10);
+            Point y(max_x+10,max_y+10);
+            Rect rect(x,y);
+            Mat cropped = result(rect);
+            cropped.copyTo(result_cropped);
+            rectangle(result,rect,color,1); // draw rectangle round the changed pixel
+        }
+        return number_of_changes;
+    }
+    return 0;
+}
+
+void parseRegionXML(string file_region, vector<Point2f> &region){
+    TiXmlDocument doc(file_region.c_str());
+    if(doc.LoadFile()) // ok file loaded correctly
+    {
+        TiXmlElement * point = doc.FirstChildElement("point");
+        int x, y;
+        while (point)
+        {
+            point->Attribute("x",&x);
+            point->Attribute("y",&y);
+            Point2f p(x,y);
+            region.push_back(p);
+            point = point->NextSiblingElement("point");
+        }
+    }
+    else
+        exit(1);
+}
+
+int main (int argc, char * const argv[])
+{
+    const string DIR = "/home/pi/homesecurity/pics/"; // directory where the images will be stored
+    const string EXT = ".jpg"; // extension of the images
+    const int DELAY = 10; // in mseconds, take a picture every 1/2 second
+    const string LOGFILE = "/home/pi/homesecurity/log";
+    
+    // Format of directory
+    string DIR_FORMAT = "%d%h%Y"; // 1Jan1970
+    string FILE_FORMAT = DIR_FORMAT + "/" + "%d%h%Y_%H%M%S"; // 1Jan1970/1Jan1970_12153
+    string CROPPED_FILE_FORMAT = DIR_FORMAT + "/cropped/" + "%d%h%Y_%H%M%S"; // 1Jan1970/cropped/1Jan1970_121539
+    
+    // Set up camera
+    CvCapture * camera = cvCaptureFromCAM(CV_CAP_ANY);
+    cvSetCaptureProperty(camera, CV_CAP_PROP_FRAME_WIDTH, 1280); // width of viewport of camera
+    cvSetCaptureProperty(camera, CV_CAP_PROP_FRAME_HEIGHT, 720); // height of ...
+    
+    // Take images and convert them to gray
+    Mat result, result_cropped;
+    Mat prev_frame = result = cvQueryFrame(camera);
+    Mat current_frame = cvQueryFrame(camera);
+    Mat next_frame = cvQueryFrame(camera);
+    cvtColor(current_frame, current_frame, CV_RGB2GRAY);
+    cvtColor(prev_frame, prev_frame, CV_RGB2GRAY);
+    cvtColor(next_frame, next_frame, CV_RGB2GRAY);
+    saveImg(result,DIR,EXT,DIR_FORMAT.c_str(),FILE_FORMAT.c_str());
+    
+    // d1 and d2 for calculating the differences
+    // result, the result of and operation, calculated on d1 and d2
+    // number_of_changes, the amount of changes in the result matrix.
+    // color, the color for drawing the rectangle when something has changed.
+    Mat d1, d2, motion;
+    int number_of_changes, number_of_sequence = 0;
+    Scalar mean_, color(0,255,255); // red
+    
+    // Detect motion in a region in steadof window
+    string file_region = "../region.xml";
+    vector<Point2f> region;
+    parseRegionXML(file_region, region);
+    
+    // If more than 'there_is_motion' pixels are changed, we say there is motion
+    // and store an image on disk
+    int there_is_motion = 5;
+    
+    // Maximum deviation of the image, the higher the value, the more motion is allowed
+    int max_deviation = 100;
+    
+    // Erode kernel
+    Mat kernel_ero = getStructuringElement(MORPH_RECT, Size(2,2));
+    
+    // All settings have been set, now go in endless loop and
+    // take as many pictures you want..
+    while (true){
+        // Take a new image
+        prev_frame = current_frame;
+        current_frame = next_frame;
+        next_frame = cvQueryFrame(camera);
+        result = next_frame;
+        cvtColor(next_frame, next_frame, CV_RGB2GRAY);
+        
+        // Calc differences between the images and do AND-operation
+        // threshold image, low differences are ignored (ex. contrast change due to sunlight)
+        absdiff(prev_frame, next_frame, d1);
+        absdiff(next_frame, current_frame, d2);
+        bitwise_and(d1, d2, motion);
+        threshold(motion, motion, 35, 255, CV_THRESH_BINARY);
+        erode(motion, motion, kernel_ero);
+        
+        number_of_changes = detectMotion(motion, result, result_cropped, region, max_deviation, color);
+        
+        // If a lot of changes happened, we assume something changed.
+        if(number_of_changes>=there_is_motion)
+        {
+            if(number_of_sequence>0){
+                saveImg(result,DIR,EXT,DIR_FORMAT.c_str(),FILE_FORMAT.c_str());
+                saveImg(result_cropped,DIR,EXT,DIR_FORMAT.c_str(),CROPPED_FILE_FORMAT.c_str());
+            }
+            number_of_sequence++;
+        }
+        else
+        {
+            number_of_sequence = 0;
+            // Delay, wait a 1/2 second.
+            cvWaitKey (DELAY);
+        }
+    }
+    return 0;
+}
